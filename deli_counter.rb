cb94bc9a6de1ba2744bfeb220be46ecd0a1a980d
Shaqqour M <mshaqqourm@gmail.com>
2019-09-14 01:36:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def line(deli_line)
+  if deli_line.size == 0
+    puts "The line is currently empty."
+    return
+  end
+  the_line = "The line is currently:"
+  deli_line.each_with_index do |person, idx|
+    the_line += " #{idx+1}. #{person}"
+  end
+  puts the_line
+end
+
+def take_a_number(deli_line, name)
+  deli_line << name
+  puts "Welcome, #{name}. You are number #{deli_line.size} in line."
+end
+
+def now_serving(deli_line)
+  if deli_line.size == 0
+  end
+end
